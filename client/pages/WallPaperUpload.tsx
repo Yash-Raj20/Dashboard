7beb8bd2575c5886e8852feca142acbe8a9d83e4
Yash-Raj20 <ratneshkumarstm987.com>
2025-09-30 03:28:20 +0530

    change the grid of wallpaper

--- client/pages/WallPaperUpload.tsx
+++ client/pages/WallPaperUpload.tsx
@@ -250,7 +250,7 @@ export default function UploadPage() {
                 No wallpapers uploaded yet.
               </p>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-2">
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
                 {wallpapers.map((wallpaper) => (
                   <div
                     key={wallpaper.id || wallpaper.title}
